734e955d3397ca3575e50554f6b857035d2a8d60
TodiTheFirst <146870436+TodiTheFirst@users.noreply.github.com>
2023-12-17 20:57:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
+// class AgedPerson {
+//     printAge() {
+//         console.log(this.age);
+//     }
+// }
+
+
+// class Person extends AgedPerson {
+//     name = 'Bibi';
+//     constructor(){
+//         super();
+//         this.age = 30;
+//     }
+//     greet = function() {
+//         console.log("Hi, I am " + this.name + " and I am " + this.age + " years old");
+//     };
+// }
+
+
+
 // class Person {
+
 //   name = "Oleg";
 
 //   constructor() {
@@ -21,13 +42,30 @@ function Person() {
 
 
 
+console.dir(Person);
 
 
-
-const p = new Person();
-p.greet();
-console.log(p.toString());
+// const p = new Person();
+// p.greet();
+// console.log(p.toString());
 //сonsole.log(p.__proto__) 
 //прототип то что скрывается за обьетами почти всё имеет прототипы
 
+// Person.prototype = {
+//     printAge() {
+//         console.log(this.age);
+//     }
+// };
+
+
+PaymentResponse.prototype.printAge = function(){
+    console.log(this.age);
+};
+ 
+const p = new Person ();
+p.greet();
+// p.printAge();
+console.log(p._proto_);
+const p2 = new p.__proto__.constructor();
+console.log(p2);
 
